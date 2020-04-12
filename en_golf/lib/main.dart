9dece75c076c540877cff72a33da388b181b33d0
AQ-TeppeiKikuchi <teppei.kikuchi@asia-quest.co.jp>
2020-04-12 13:49:47 +0900

    不要処理の削除

--- en_golf/lib/main.dart
+++ en_golf/lib/main.dart
@@ -241,29 +241,6 @@ class NewsTab extends StatelessWidget {
     );
   }
 
-//  void _showModalPicker(BuildContext context) {
-//    showModalBottomSheet<void>(
-//      context: context,
-//      builder: (BuildContext context) {
-//        return Container(
-//          height: MediaQuery.of(context).size.height / 3,
-//          child: GestureDetector(
-//            onTap: () {
-//              Navigator.pop(context);
-//            },
-//            child: CupertinoPicker(
-//              itemExtent: 40,
-//              children: _items.map(_pickerItem).toList(),
-//              onSelectedItemChanged: ((index) {
-//                olympicBloc.changePlayerCountAction.add(_items[index]);
-//              }),
-//            ),
-//          ),
-//        );
-//      },
-//    );
-//  }
-
   String _selectedItem = 'none';
 
   final List<int> _items = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50];
