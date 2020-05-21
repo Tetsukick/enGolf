bbb9447b337cce160b798d1a45b7084b0f89546a
AQ-TeppeiKikuchi <teppei.kikuchi@asia-quest.co.jp>
2020-05-21 18:23:54 +0900

    乱数生成画面の生成

--- en_golf/lib/dice_bloc.dart
+++ en_golf/lib/dice_bloc.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:rxdart/rxdart.dart';
+import 'dart:math';
 
 class DiceBloc {
   // input
@@ -8,17 +9,32 @@ class DiceBloc {
   Sink<RangeValues> get changeRangeAction => _rangeController.sink;
   Stream<RangeValues> get range => _rangeController.stream;
 
+  final _historyController = BehaviorSubject<List<int>>.seeded([]);
+  Stream<List<int>> get histories => _historyController.stream;
+
   RangeValues _range = RangeValues(1, 18);
+  List<int> _histories = [];
 
   DiceBloc() {
-
     _listenRange();
+//    _historyController.add(_histories);
   }
 
   void _listenRange() {
     _rangeController.stream.listen((newRange) {
       _range = newRange;
     });
+  }
+
+  void createRandomNumber() {
+    final numOfRange = _range.end.round() - _range.start.round() + 1;
+    final List<int> numbers = List.generate(numOfRange, (i) => i + _range.start.round());
+
+    var rand = new Random();
+    int lottery = rand.nextInt(numOfRange);
+
+    _histories.add(numbers[lottery]);
+    _historyController.add(_histories);
   }
 
   void dispose() {
--- en_golf/lib/dice_screen.dart
+++ en_golf/lib/dice_screen.dart
@@ -12,121 +12,151 @@ import 'constants.dart' as Constants;
 class DiceScreen extends StatelessWidget {
   final double min = 1;
   final double max = 18;
+  final ScrollController _scrollController = ScrollController();
 
   @override
   Widget build(BuildContext context) {
     final diceBloc = Provider.of<DiceBloc>(context);
 
     return Scaffold(
-      body: Container(
-        child: StreamBuilder(
-            stream: diceBloc.range,
-            builder: (context, snapshot) {
-              if (snapshot.data == null) {
-                print(snapshot);
-                print('null');
-                return Center(
-                  child: Text('error'),
-                );
-              } else {
-                RangeValues _range = snapshot.data;
-                return Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: <Widget>[
-                    SizedBox(height: 50,),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceAround,
-                      children: <Widget>[
-                        Text(
-                          _range.start.round().toString(),
-                          style: TextStyle(fontSize: 30),
-                        ),
-                        SliderTheme(
-                          data: SliderThemeData(
-                            activeTrackColor: Colors.lightGreen,
-                            showValueIndicator: ShowValueIndicator.always,
-                          ),
-                          child: RangeSlider(
-                            labels: RangeLabels(_range.start.round().toString(), _range.end.round().toString()),
-                            values: _range,
-                            min: min,
-                            max: max,
-                            onChanged: (values) {
-                              diceBloc.changeRangeAction.add(values);
-                            },
-                          ),
-                        ),
-                        Text(
-                          _range.end.round().toString(),
-                          style: TextStyle(fontSize: 30),
-                        ),
-                      ],
-                    ),
-                  ],
-                );
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: <Widget>[
+          _createSlider(context),
+          _createHistoryView(context: context, controller: _scrollController),
+          StreamBuilder(
+              stream: diceBloc.histories,
+              builder: (context, snapshot) {
+                List<int> _histories = snapshot.data;
+                if (_histories == null || _histories.length == 0) {
+                  return Text(
+                    "Shake!!",
+                    style: TextStyle(fontSize: 30),
+                  );
+                } else {
+                  return _createGolfBallView(num: _histories.last, height: 150, width: 150);
+                }
               }
-            }
-        ),
+          ),
+          RaisedButton(
+            child: Padding(
+              padding: EdgeInsets.all(10),
+              child: Text(
+                "Shake!!",
+                style: TextStyle(
+                  fontSize: 30,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+            color: Colors.lightGreen,
+            shape: StadiumBorder(),
+            onPressed: () {
+              diceBloc.createRandomNumber();
+            },
+          ),
+        ],
       ),
     );
   }
 
-//  Widget createSlider(BuildContext context) {
-//    final diceBloc = Provider.of<DiceBloc>(context);
-//
-//    return Container(
-//      child: StreamBuilder(
-//        stream: diceBloc.range,
-//        builder: (context, snapshot) {
-//          if (snapshot.data == null) {
-//            print('null');
-//            return Center(
-//              child: Text('error'),
-//            );
-//          } else {
-//            RangeValues _range = snapshot.data;
-//            return Column(
-//              mainAxisAlignment: MainAxisAlignment.start,
-//              children: <Widget>[
-//                SizedBox(height: 50,),
-//                SliderTheme(
-//                  data: SliderThemeData(
-//                    activeTrackColor: Colors.red,
-//                    showValueIndicator: ShowValueIndicator.always,
-//                  ),
-//                  child: RangeSlider(
-//                    labels: RangeLabels(_range.start.toString(), _range.end.toString()),
-//                    values: _range,
-//                    min: 1,
-//                    max: 100,
-//                    divisions: 100,
-//                    onChanged: (values) {
-//                      diceBloc.changeRangeAction.add(values);
-//                    },
-//                  ),
-//                ),
-//                Container(
-//                  padding: EdgeInsets.all(20),
-//                  child: Row(
-//                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-//                    children: <Widget>[
-//                      Text(
-//                        _range.start.toString(),
-//                        style: TextStyle(fontSize: 40),
-//                      ),
-//                      Text(
-//                        _range.end.toString(),
-//                        style: TextStyle(fontSize: 40),
-//                      ),
-//                    ],
-//                  ),
-//                )
-//              ],
-//            );
-//          }
-//        }
-//      ),
-//    );
-//  }
+  Widget _createSlider(BuildContext context) {
+    final diceBloc = Provider.of<DiceBloc>(context);
+
+    return Container(
+      child: StreamBuilder(
+          stream: diceBloc.range,
+          builder: (context, snapshot) {
+            if (snapshot.data == null) {
+              return Center(
+                child: Text('error'),
+              );
+            } else {
+              RangeValues _range = snapshot.data;
+              return Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: <Widget>[
+                  SizedBox(height: 50,),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: <Widget>[
+                      Text(
+                        _range.start.round().toString(),
+                        style: TextStyle(fontSize: 30),
+                      ),
+                      SliderTheme(
+                        data: SliderThemeData(
+                          activeTrackColor: Colors.lightGreen,
+                          showValueIndicator: ShowValueIndicator.always,
+                        ),
+                        child: RangeSlider(
+                          labels: RangeLabels(_range.start.round().toString(), _range.end.round().toString()),
+                          values: _range,
+                          min: min,
+                          max: max,
+                          onChanged: (values) {
+                            diceBloc.changeRangeAction.add(values);
+                          },
+                        ),
+                      ),
+                      Text(
+                        _range.end.round().toString(),
+                        style: TextStyle(fontSize: 30),
+                      ),
+                    ],
+                  ),
+                ],
+              );
+            }
+          }
+      ),
+    );
+  }
+
+  Widget _createHistoryView({BuildContext context, ScrollController controller}) {
+    final diceBloc = Provider.of<DiceBloc>(context);
+
+    return StreamBuilder(
+        stream: diceBloc.histories,
+        builder: (context, snapshot) {
+          List<int> _histories = snapshot.data;
+          if (_histories == null || _histories.length == 0) {
+            return Container();
+          } else {
+            return Center(
+              child: Container(
+                height: 50,
+                child: ListView.builder(
+                  controller: controller,
+                  scrollDirection: Axis.horizontal,
+                  itemCount: _histories.length,
+                  itemBuilder: (context, int index) {
+                    return _createGolfBallView(num: _histories[index], height: 50, width: 50);
+                  }),
+              ),
+            );
+          }
+        }
+    );
+  }
+
+  Widget _createGolfBallView({int num, double height, double width}) {
+    return Container(
+      height: height,
+      width: width,
+      decoration: BoxDecoration(
+          image: DecorationImage(
+              fit: BoxFit.fill,
+              image: AssetImage("assets/golf_ball.png")
+          )
+      ),
+      child: Center(
+        child: Text(
+          num.toString(),
+          style: TextStyle(fontSize: height/2),
+        ),
+      ),
+    );
+  }
 
 }
