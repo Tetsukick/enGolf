9f5c08d113bf48645bba889133fd72be46a815b1
AQ-TeppeiKikuchi <teppei.kikuchi@asia-quest.co.jp>
2020-05-30 01:43:40 +0900

    ListViewのアニメーションの追加

--- en_golf/lib/olympic_screen.dart
+++ en_golf/lib/olympic_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_staggered_animations/flutter_staggered_animations.dart';
 
 import 'package:provider/provider.dart';
 
@@ -114,15 +115,27 @@ class OlympicScreen extends StatelessWidget {
                 return Container();
               } else {
                 List<Player> players = snapshot.data;
-                return ListView.builder(
+                return AnimationLimiter(
+                  child: ListView.builder(
                     itemCount: players.length,
-                    itemBuilder: (context, int index) {
-                      return SafeArea(
-                          top: false,
-                          bottom: false,
-                          child: ScoreCard(color: Constants.colors[players[index].rank], player: players[index],)
+                    itemBuilder: (BuildContext context, int index) {
+                      return AnimationConfiguration.staggeredList(
+                        position: index,
+                        duration: const Duration(milliseconds: 375),
+                        child: SlideAnimation(
+                          verticalOffset: 50.0,
+                          child: FadeInAnimation(
+                            child: SafeArea(
+                                top: false,
+                                bottom: false,
+                                child: ScoreCard(color: Constants.colors[players[index].rank], player: players[index],)
+                            ),
+                          ),
+                        ),
                       );
-                    });
+                    },
+                  ),
+                );
               }
             }),
       ),
