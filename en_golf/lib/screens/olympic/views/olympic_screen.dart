2a22cbac43ae4c0278006228e7d71b524e8ad52d
AQ-TeppeiKikuchi <teppei.kikuchi@asia-quest.co.jp>
2021-05-13 19:49:26 +0700

    temp layout

--- en_golf/lib/screens/olympic/views/olympic_screen.dart
+++ en_golf/lib/screens/olympic/views/olympic_screen.dart
@@ -19,95 +19,67 @@ class OlympicScreen extends StatelessWidget {
 
   @override
   Widget build(context) {
+    final olympicBloc = Provider.of<OlympicBloc>(context);
     return Container(
       padding: EdgeInsets.all(SizeConfig.mediumMargin),
       color: ColorConfig.bgGreenPrimary,
       child: Stack(
         children: [
           Column(
+            mainAxisSize: MainAxisSize.min,
             children: [
               header(context),
+              Expanded(
+                child: StreamBuilder(
+                    stream: olympicBloc.players,
+                    builder: (context, snapshot) {
+                      if (snapshot.data == null) {
+                        return Container();
+                      } else {
+                        final players = snapshot.data as List<Player>;
+                        return AnimationLimiter(
+                          child: Padding(
+                            padding: const EdgeInsets.only(bottom: 55),
+                            child: ListView.builder(
+                              itemCount: players.length,
+                              itemBuilder: (BuildContext context, int index) {
+                                return AnimationConfiguration.staggeredList(
+                                  position: index,
+                                  duration: const Duration(milliseconds: 375),
+                                  child: SlideAnimation(
+                                    verticalOffset: 50.0,
+                                    child: FadeInAnimation(
+                                      child: SafeArea(
+                                          top: false,
+                                          bottom: true,
+                                          child: ScoreCard(color: Constants.colors[players[index].rank], player: players[index],)
+                                      ),
+                                    ),
+                                  ),
+                                );
+                              },
+                            ),
+                          ),
+                        );
+                      }
+                    }
+                ),
+              ),
             ],
           ),
-//        CustomScrollView(slivers: <Widget>[
-//        SliverAppBar(
-//          pinned: true,
-//          expandedHeight: 150,
-//          collapsedHeight: 100,
-//          flexibleSpace: FlexibleSpaceBar(
-//            titlePadding: const EdgeInsets.all(0),
-//            title: Row(
-//              crossAxisAlignment: CrossAxisAlignment.start,
-//              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-//              mainAxisSize: MainAxisSize.min,
-//              children: <Widget>[
-//                titleTextField(
-//                    context,
-//                    AppLocalizations.of(context).rate,
-//                    olympicBloc.rate,
-//                    olympicBloc.changeRateAction.add),
-//                titleTextField(
-//                    context,
-//                    AppLocalizations.of(context).player,
-//                    olympicBloc.playerCount,
-//                    olympicBloc.changePlayerCountAction.add),
-//              ],
-//            ),
-//            background: Image.asset('assets/top-background.jpg', fit: BoxFit.cover),
-//          ),
-//        ),
-//        SliverFillRemaining(child:
-//          CupertinoPageScaffold(
-//            child: StreamBuilder(
-//                    stream: olympicBloc.players,
-//                    builder: (context, snapshot) {
-//                      if (snapshot.data == null) {
-//                        return Container();
-//                      } else {
-//                        final players = snapshot.data as List<Player>;
-//                        return AnimationLimiter(
-//                          child: Padding(
-//                            padding: const EdgeInsets.only(bottom: 55),
-//                            child: ListView.builder(
-//                              itemCount: players.length,
-//                              itemBuilder: (BuildContext context, int index) {
-//                                return AnimationConfiguration.staggeredList(
-//                                  position: index,
-//                                  duration: const Duration(milliseconds: 375),
-//                                  child: SlideAnimation(
-//                                    verticalOffset: 50.0,
-//                                    child: FadeInAnimation(
-//                                      child: SafeArea(
-//                                          top: false,
-//                                          bottom: true,
-//                                          child: ScoreCard(color: Constants.colors[players[index].rank], player: players[index],)
-//                                      ),
-//                                    ),
-//                                  ),
-//                                );
-//                              },
-//                            ),
-//                          ),
-//                        );
-//                      }
-//                    }
-//                ),
-//          ),
-//        ),
-//      ],
-//      ),
-        Positioned(
-          left: 0,
-          right: 0,
-          bottom: 0,
-          child: Container(
-            height: 50,
-            child: AdmobBanner(
-              adUnitId: getBannerAdUnitId(),
-              adSize: AdmobBannerSize.LEADERBOARD,
+          Positioned(
+            left: 0,
+            right: 0,
+            bottom: 0,
+            child: Container(
+              height: 50,
+              child: AdmobBanner(
+                adUnitId: getBannerAdUnitId(),
+                adSize: AdmobBannerSize.LEADERBOARD,
+              ),
             ),
-          ),
-        )]
+          )
+        ]
       ),
     );
   }
