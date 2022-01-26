97a9f80a7a4d735e1600774e50d8b6f355f82ffe
AQ-TeppeiKikuchi <teppei.kikuchi@asia-quest.co.jp>
2022-01-26 12:06:54 +0900

    android スプラッシュGIF追加

--- en_golf/android/app/src/main/kotlin/com/tetsukick/engolf/MainActivity.kt
+++ en_golf/android/app/src/main/kotlin/com/tetsukick/engolf/MainActivity.kt
@@ -4,9 +4,14 @@ import androidx.annotation.NonNull;
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugins.GeneratedPluginRegistrant
+import io.flutter.embedding.android.SplashScreen
+
+import SplashView
 
 class MainActivity: FlutterActivity() {
     override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
         GeneratedPluginRegistrant.registerWith(flutterEngine);
     }
+
+    override fun provideSplashScreen(): SplashScreen? = SplashView()
 }
